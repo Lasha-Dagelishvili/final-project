7c3f5b7280f52bac64f46b298055c70a4965149e
Lasha-Dagelishvili <dagelishvililasha@gmail.com>
2025-01-13 15:02:47 +0400

    created vite project

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        ],
+        parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        },
+        settings: {
+        react: {
+            version: 'detect',
+        },
+        },
+        rules: {},
+    };
+    
